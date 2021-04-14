f1b97e7143e87d8445027c14535e49c418fdf76e
Gilberto Silva <gilberto.silva@sigga.com>
2021-04-14 19:26:45 -0300

    Criando projeto.

---
+++ Firebase Aula/Firebase Aula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase Aula
+//
+//  Created by Gilberto Silva on 14/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
