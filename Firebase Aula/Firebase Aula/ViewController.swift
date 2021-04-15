9199e0409416da1cb01bef50ac9e89cdf88851aa
Gilberto Silva <gilberto.silva@sigga.com>
2021-04-15 15:12:11 -0300

    Criando um novo usuário no banco de dados do firebase.

--- Firebase Aula/Firebase Aula/ViewController.swift
+++ Firebase Aula/Firebase Aula/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    let firebaseDatabase = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createNewUser()
     }
-
-
+    
+    private func createNewUser(){
+        let usersChild = firebaseDatabase.child("users")
+        let userNew = usersChild.child("005")
+        userNew.child("name").setValue("José")
+        userNew.child("last_name").setValue("Moreira")
+        userNew.child("email").setValue("jose@gmail.com")
+    }
+    
 }
 
